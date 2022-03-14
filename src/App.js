e82b4a4e4f373e612591153771243cf143df18e0
arunkanand6 <arunkanand6@gmail.com>
2022-03-14 15:51:11 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import AppRouter from "./Router/AppRouter";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import { useEffect } from "react";
-import Logo from "./assets/logo.png";
 
 function App() {
   useEffect(() => {
