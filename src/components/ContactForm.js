bab63a001d3a8b14d8a24a5e50165e0f2b38c6be
arunkanand6 <arunkanand6@gmail.com>
2022-03-28 13:56:10 +0530

    .

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,9 +1,6 @@
 import { useState } from "react";
-import { useNavigate } from "react-router";
 
 function ContactForm() {
-  const navigate = useNavigate();
-
   const [data, setData] = useState({
     order: "",
   });
