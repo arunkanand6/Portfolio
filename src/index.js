92004f1886fe0543fcef0443ee13235426bb0483
arunkanand6 <arunkanand6@gmail.com>
2022-03-28 10:25:03 +0530

    .

--- src/index.js
+++ src/index.js
@@ -4,10 +4,50 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-<form name="contact" netlify netlify-honeypot="bot-field" hidden>
-  <input type="text" name="name" />
-  <input type="email" name="email" />
-  <textarea name="message"></textarea>
+<form onSubmit="Submit">
+  <input className="d-none" name="botfield" />
+  <label htmlFor="name" className="my-2">
+    Name
+  </label>
+  <input
+    className="my-2 myinput"
+    type="text"
+    name="name"
+    id="name"
+    placeholder="enter your name here"
+  />
+  {<div className="mywarning">Name should be 3 to 20 characters</div>}
+  <label htmlFor="mobile" className="my-2">
+    Mobile No
+  </label>
+  <input
+    className="my-2  myinput"
+    type="tel"
+    name="mobile"
+    id="mobile"
+    placeholder="enter your mobile number"
+  />
+  {<div className="mywarning">Enter a valid mobile number</div>}
+  <label htmlFor="email" className="my-2">
+    Email
+  </label>
+  <input
+    className="my-2  myinput"
+    type="email"
+    name="email"
+    id="email"
+    placeholder="example@address.com"
+  />
+  {<div className="mywarning">Enter a valid email id</div>}
+  <label htmlFor="message" className="my-2">
+    Message
+  </label>
+  <textarea name="message" id="message" className="mytextarea my-2"></textarea>
+  <div className="text-center">
+    <button className="btn btn-primary my-2" type="submit">
+      Submit
+    </button>
+  </div>
 </form>;
 
 ReactDOM.render(
